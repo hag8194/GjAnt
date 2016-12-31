d47fbf3ecb6267ce5dc46bd30a9b447e06901ab1
Hugo Giordano <hag8194@gmail.com>
2016-12-31 17:11:33 -0400

    First commit

--- data/src/main/java/com/gjdev/gjant/data/api/ApiConstants.java
+++
@@ -1,6 +0,0 @@
-package com.gjdev.gjant.data.api;
-
-public class ApiConstants {
-
-  public static final String BASE_URL = "https://doodle-manga-scraper.p.mashape.com";
-}
--- domain/src/main/java/com/gjdev/gjant/domain/executor/ThreadExecutor.java
+++
@@ -1,8 +0,0 @@
-package com.gjdev.gjant.domain.executor;
-
-import java.util.concurrent.Executor;
-
-
-public interface ThreadExecutor {
-    Executor execute();
-}
